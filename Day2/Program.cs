c276625a76dbb1468edd0ad73c0ff953f3250968
Phoebe Mitchell <phoebe.mitchell@gmsl.co.uk>
2023-09-21 11:52:56 +0100

    Day2

---
+++ Day2/Program.cs
@@ -0,0 +1,29 @@
+﻿const int winScore = 6;
+
+var scores = new Dictionary<char, int>()
+{
+    { 'X', 1 },
+    { 'Y', 2 },
+    { 'Z', 3 }
+};
+
+var winningCombinations = new[]
+{
+    ('A', 'Y'),
+    ('B', 'Z'),
+    ('C', 'X')
+};
+
+var score = 0;
+using (var streamReader = new StreamReader("input.txt")) {
+    while (streamReader.ReadLine() is { } line)
+    {
+        if (winningCombinations.Contains((line[0], line[2])))
+        {
+            score += winScore;
+        }
+
+        score += scores[line[2]];
+    }
+}
+Console.WriteLine(score);
