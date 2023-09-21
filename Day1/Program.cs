5b17b82e22767321bb7b1070b80e667d92ee9194
Phoebe Mitchell <phoebe.mitchell@gmsl.co.uk>
2023-09-21 09:29:59 +0100

    Day1

---
+++ Day1/Program.cs
@@ -0,0 +1,27 @@
+﻿var elfCalorieList = new List<int>
+{
+    new ()
+};
+using (var streamReader = new StreamReader("input.txt"))
+{
+    while (streamReader.ReadLine() is { } line) {
+        if (line == "")
+        {
+            elfCalorieList.Add(new());
+        }
+        else
+        {
+            elfCalorieList[^1] += int.Parse(line);
+        } 
+    }
+}
+
+var elfWithMostCalories = 0;
+for (var i = 0; i < elfCalorieList.Count; i++)
+{
+    if (elfCalorieList[i] > elfCalorieList[elfWithMostCalories])
+    {
+        elfWithMostCalories = i;
+    }
+}
+Console.WriteLine($"Elf {elfWithMostCalories + 1} has the most calories: {elfCalorieList[elfWithMostCalories]}.");
