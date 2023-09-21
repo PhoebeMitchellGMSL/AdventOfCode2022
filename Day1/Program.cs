53cc1a7303ea1e8077e4a7020b618a7fe5c79ff5
Phoebe Mitchell <phoebe.mitchell@gmsl.co.uk>
2023-09-21 08:56:04 +0100

    backup

---
+++ Day1/Program.cs
@@ -0,0 +1,7 @@
+﻿var streamReader = new StreamReader("input.txt");
+var line = streamReader.ReadLine();
+while (line == null)
+{
+    Console.WriteLine(line);
+    line = streamReader.ReadLine();
+}
