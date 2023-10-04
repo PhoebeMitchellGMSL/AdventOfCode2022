6625b06621ae95eb7865fe1e5ec3e6782056caf5
Phoebe Mitchell <phoebe.mitchell@gmsl.co.uk>
2023-10-04 14:14:16 +0100

    Part 2

--- Day4/Program.cs
+++ Day4/Program.cs
@@ -1,5 +1,10 @@
 ﻿var lines = File.ReadAllLines("input.txt");
-var a = lines.Select(line => line.Split(",").Select(section => section.Split("-").Select(s => int.Parse(s)).ToArray())
-                .ToArray()).Count(line => line[0][0] >= line[1][0] && line[0][1] <= line[1][1] || line[1][0] >= line[0][0] && line[1][1] <= line[0][1]);
+var parsedLines = lines.Select(line => line.Split(",").Select(section => section.Split("-").Select(int.Parse).ToArray()).ToArray()).ToArray();
 
-Console.WriteLine(a);
+// Part 1
+var part1 = parsedLines.Select(line => line.OrderBy(value => value[1] - value[0]).ToArray()).Count(line => line[0][0] >= line[1][0] && line[0][1] <= line[1][1]);
+Console.WriteLine(part1);
+
+// Part 2
+var part2 = parsedLines.Select(line => line.OrderBy(value => value[0]).ToArray()).Count(line => line[0][1] >= line[1][0]);
+Console.WriteLine(part2);
