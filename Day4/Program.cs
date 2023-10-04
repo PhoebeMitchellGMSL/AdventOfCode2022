ba3c63f02cd7d1f631b8b6ea305d64c250a50646
Phoebe Mitchell <phoebe.mitchell@gmsl.co.uk>
2023-10-04 10:29:44 +0100

    Day4 Part 1

---
+++ Day4/Program.cs
@@ -0,0 +1,5 @@
+﻿var lines = File.ReadAllLines("input.txt");
+var a = lines.Select(line => line.Split(",").Select(section => section.Split("-").Select(s => int.Parse(s)).ToArray())
+                .ToArray()).Count(line => line[0][0] >= line[1][0] && line[0][1] <= line[1][1] || line[1][0] >= line[0][0] && line[1][1] <= line[0][1]);
+
+Console.WriteLine(a);
