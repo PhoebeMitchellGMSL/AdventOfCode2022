b0daa8a2326980e8aa47b0e0a5af364080d2c5eb
Phoebe Mitchell <phoebe.mitchell@gmsl.co.uk>
2023-10-18 10:03:35 +0100

    Day6

---
+++ Day6/Program.cs
@@ -0,0 +1,13 @@
+﻿var input = File.ReadAllLines("input.txt")[0];
+
+// Set marker to 4 for part 1, 14 for part 2
+const int marker = /*4*/14;
+
+for (var i = 0; i < input.Length; i++)
+{
+    if (input.Substring(i, marker).Distinct().Count() == marker)
+    {
+        Console.WriteLine(i + marker);
+        break;
+    }
+}
