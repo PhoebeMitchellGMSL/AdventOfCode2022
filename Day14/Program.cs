99747c3ecfa05bce2befb8c5fa8e3143f61bf24f
Phoebe Mitchell <phoebe.mitchell@gmsl.co.uk>
2023-12-18 11:44:37 +0000

    Day 14

--- Day14/Program.cs
+++ Day14/Program.cs
@@ -3,9 +3,10 @@
 var lines = File.ReadAllLines("input.txt");
 // var lines = new string[]
 // {
-//     "0,0 -> 10,0",
-//     "10,0 -> 10,10" 
+//     "0,10 -> 10,10",
+//     "5,5 -> 5,10" 
 // };
+var minPosition = new Vector2(0, 0);
 var maxPosition = new Vector2();
 
 var sequences = new Vector2[lines.Length][];
@@ -18,19 +19,16 @@ for (var i = 0; i < sequences.Length; i++)
     {
         var vectorValues = vectors[j].Split(",");
         sequences[i][j] = new Vector2(int.Parse(vectorValues[0]), int.Parse(vectorValues[1]));
+        minPosition.X = Math.Min(sequences[i][j].X, minPosition.X);
+        minPosition.Y = Math.Min(sequences[i][j].Y, minPosition.Y);
         maxPosition.X = Math.Max(sequences[i][j].X, maxPosition.X);
         maxPosition.Y = Math.Max(sequences[i][j].Y, maxPosition.Y);
     }
 }
 
-var grid = new char[(int)maxPosition.X + 1, (int)maxPosition.Y + 1];
-for (var i = 0; i < maxPosition.X + 1; i++)
-{
-    for (var j = 0; j < maxPosition.Y + 1; j++)
-    {
-        grid[i, j] = '.';
-    }
-}
+maxPosition.Y += 1;
+
+var grid = new Dictionary<Vector2, char>();
 
 foreach (var sequence in sequences)
 {
@@ -39,20 +37,61 @@ foreach (var sequence in sequences)
         var difference = sequence[i + 1] - sequence[i];
         var normalisedDifference = new Vector2(Math.Sign(difference.X), Math.Sign(difference.Y));
         var startPosition = sequence[i];
-        grid[(int)startPosition.X, (int)startPosition.Y] = '#';
+        grid.TryAdd(startPosition, '#');
         while (startPosition != sequence[i + 1])
         {
             startPosition += normalisedDifference;
-            grid[(int)startPosition.X, (int)startPosition.Y] = '#';
+            grid.TryAdd(startPosition, '#');
         }
     }
 }
 
+while (true)
+{
+    var currentPosition = new Vector2(500, 0);
+    
+    if (grid.TryGetValue(currentPosition, out _))
+    {
+        break;
+    }
+    
+    while (true)
+    {
+        if (currentPosition.Y + 1 > maxPosition.Y)
+        {
+        }
+        else if (!grid.TryGetValue(currentPosition + new Vector2(0, 1), out _))
+        {
+            currentPosition += new Vector2(0, 1);
+            continue;
+        }
+        else if (!grid.TryGetValue(currentPosition + new Vector2(-1, 1), out _))
+        {
+            currentPosition += new Vector2(-1, 1);
+            continue;
+        }
+        else if (!grid.TryGetValue(currentPosition + new Vector2(1, 1), out _))
+        {
+            currentPosition += new Vector2(1, 1);
+            continue;
+        }
+        
+        grid.Add(currentPosition, '+');
+        
+        break;
+    }
+    
+    minPosition.X = Math.Min(minPosition.X, currentPosition.X);
+    maxPosition.X = Math.Max(maxPosition.X, currentPosition.X);
+}
+
 for (var j = 0; j < maxPosition.Y + 1; j++)
 {
-    for (var i = 0; i < maxPosition.X + 1; i++)
+    for (var i = minPosition.X; i < maxPosition.X + 1; i++)
     {
-        Console.Write(grid[i,j]);
+        Console.Write(grid.GetValueOrDefault(new Vector2(i, j), '.'));
     }
     Console.WriteLine();
 }
+
+Console.WriteLine(grid.Values.Count(value => value == '+'));
