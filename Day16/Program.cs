fb37714bc50be231390ee2ef4315313e5621793e
Phoebe Mitchell <phoebe.mitchell@gmsl.co.uk>
2024-01-03 11:49:05 +0000

    Day 16 part 2
    Revert "backup"
    
    This reverts commit 01e4710a142ec2544dedcbd199ae3927957cc3fb.
    
    backup

--- Day16/Program.cs
+++ Day16/Program.cs
@@ -42,20 +42,74 @@ foreach (var x in valveFlowRates.Keys)
 }
 
 var interestedValves = valveFlowRates.Where(pair => pair.Value != 0 || pair.Key == "AA").Select(pair => pair.Key);
-var pressures = new List<int>();
-Evaluate("AA", 30, 0, [..interestedValves]);
-Console.Write(pressures.Max());
 
+var pressuresAndValves = new List<(int, List<string>)>();
+
+Evaluate("AA", 26, 0, [..interestedValves], []);
+// Console.WriteLine(pressuresAndValves.Max());
+
+var results = pressuresAndValves.Where(x => x.Item2.Count != 1).OrderByDescending(x => x.Item1).ToList();
+
+var best = (0, 0, new List<string>(), new List<string>());
+
+var counter = 0;
+foreach (var result in results)
+{
+    foreach (var r in results)
+    {
+        if (result == r)
+        {
+            continue;
+        }
+
+        if (result.Item1 + r.Item1 <= best.Item1 + best.Item2)
+        {
+            continue;
+        }
+        
+        if (result.Item2.All(x => r.Item2.All(y => x != y)))
+        {
+            best = (result.Item1, r.Item1, result.Item2, r.Item2);
+        }
+    }
+    counter++;
+    if (counter > 3000)
+    {
+        break;
+    }
+}
+
+foreach (var valve in best.Item3)
+{
+    Console.Write($"{valve} ");
+}
+Console.WriteLine();
+foreach (var valve in best.Item4)
+{
+    Console.Write($"{valve} ");
+}
+Console.WriteLine();
+Console.WriteLine($"{best.Item1} + {best.Item2} = {best.Item1 + best.Item2}");
 return;
 
-void Evaluate(string id, int time, int pressure, List<string> remainingValves)
+void AddPressure(int pressure, List<string> visited)
+{
+    pressuresAndValves.Add((pressure, [..visited]));
+}
+
+void Evaluate(string id, int time, int pressure, List<string> remainingValves, List<string> visited)
 {
     if (time <= 0)
     {
-        pressures.Add(pressure);
+        AddPressure(pressure, visited);
         return;
     }
 
+    if (id != "AA")
+    {
+        visited.Add(id);
+    }
+    
     if (valveFlowRates[id] > 0)
     {
         pressure += valveFlowRates[id] * (time - 1);
@@ -64,7 +118,7 @@ void Evaluate(string id, int time, int pressure, List<string> remainingValves)
 
     if (time <= 0)
     {
-        pressures.Add(pressure);
+        AddPressure(pressure, visited);
         return;
     }
 
@@ -72,11 +126,16 @@ void Evaluate(string id, int time, int pressure, List<string> remainingValves)
     
     if (remainingValves.Count == 0)
     {
-        pressures.Add(pressure);
+        AddPressure(pressure, visited);
     }
     
     foreach (var v in remainingValves)
     {
-        Evaluate(v, time - matrix[id][v], pressure, [..remainingValves]);
+        if (id != "AA")
+        {
+            AddPressure(pressure, visited);
+        }
+
+        Evaluate(v, time - matrix[id][v], pressure, [..remainingValves], [..visited]);
     }
 }
